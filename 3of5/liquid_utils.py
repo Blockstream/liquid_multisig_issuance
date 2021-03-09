d0e473871bb7c096dff2baa00f5331022bd7730f
Leonardo Comandini <leo@leocomandini.org>
2021-03-09 11:44:14 +0100

    Replace outdated URLs

--- 3of5/liquid_utils.py
+++ 3of5/liquid_utils.py
@@ -5,15 +5,15 @@ network = 'regtest'
 
 def connect():
     if network == 'regtest':
-        serverURL_1 = 'http://user1:pwd1@ijxvvsti25jg6fm6.onion:18891/wallet/wallet_1.dat'
-        serverURL_2 = 'http://user1:pwd1@ijxvvsti25jg6fm6.onion:18891/wallet/wallet_2.dat'
-        serverURL_3 = 'http://user1:pwd1@ijxvvsti25jg6fm6.onion:18891/wallet/wallet_3.dat'
-        serverURL_4 = 'http://user1:pwd1@ijxvvsti25jg6fm6.onion:18891/wallet/wallet_4.dat'
-        serverURL_5 = 'http://user1:pwd1@ijxvvsti25jg6fm6.onion:18891/wallet/wallet_5.dat'
-        serverURL_6 = 'http://user1:pwd1@ijxvvsti25jg6fm6.onion:18891/wallet/wallet_6.dat'
-        serverURL_7 = 'http://user1:pwd1@ijxvvsti25jg6fm6.onion:18891/wallet/wallet_7.dat'
-        serverURL_8 = 'http://user1:pwd1@ijxvvsti25jg6fm6.onion:18891/wallet/wallet_8.dat'
-        serverURL_9 = 'http://user1:pwd1@ijxvvsti25jg6fm6.onion:18891/wallet/wallet_9.dat'
+        serverURL_1 = 'http://username:password@127.0.0.1:18888/wallet/wallet_1.dat'
+        serverURL_2 = 'http://username:password@127.0.0.1:18888/wallet/wallet_2.dat'
+        serverURL_3 = 'http://username:password@127.0.0.1:18888/wallet/wallet_3.dat'
+        serverURL_4 = 'http://username:password@127.0.0.1:18888/wallet/wallet_4.dat'
+        serverURL_5 = 'http://username:password@127.0.0.1:18888/wallet/wallet_5.dat'
+        serverURL_6 = 'http://username:password@127.0.0.1:18888/wallet/wallet_6.dat'
+        serverURL_7 = 'http://username:password@127.0.0.1:18888/wallet/wallet_7.dat'
+        serverURL_8 = 'http://username:password@127.0.0.1:18888/wallet/wallet_8.dat'
+        serverURL_9 = 'http://username:password@127.0.0.1:18888/wallet/wallet_9.dat'
     else:
         serverURL_1 = 'http://username:password@127.0.0.1:7041/wallet/wallet_1.dat'
         serverURL_2 = 'http://username:password@127.0.0.1:7041/wallet/wallet_2.dat'
@@ -41,7 +41,7 @@ def connect():
 def generate():
     time.sleep(5)
     if network == 'regtest':
-        serverURL = 'http://user1:pwd1@ijxvvsti25jg6fm6.onion:18891/wallet/wallet.dat'
+        serverURL = 'http://username:password@127.0.0.1:18888/wallet/wallet.dat'
         host = RPCHost(serverURL)
         address = host.call('getnewaddress')
         return(host.call('generatetoaddress', 1, address))
